a921fc28e1a0896961a4cc97628cb478210047d9
abdulahad934 <adbulahad934@gmail.com>
2026-01-13 16:22:08 +0600

    utils added

--- us_visa/logger/__init__.py
+++ us_visa/logger/__init__.py
@@ -1,17 +1,16 @@
 import logging
 import os
-
-from from_root import from_root
 from datetime import datetime
+from from_root import from_root
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 log_dir = "logs"
+log_dir_path = os.path.join(from_root(), log_dir)
 
-logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
+os.makedirs(log_dir_path, exist_ok=True)
 
-os.makedirs(log_dir, exist_ok=True)
-
+logs_path = os.path.join(log_dir_path, LOG_FILE)
 
 logging.basicConfig(
     filename=logs_path,
